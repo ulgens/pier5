90e9f9c06307afb2462999c7b66b098103cbf9fb
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2026-08-26 18:59:12 +0300

    Remove mixins from py5Sketch
    Mixins will be added to Sketch class later, when needed. This approach provides an easier way to eliminate unwanted mixins - behaviours.

--- src/pier5/sketch.py
+++ src/pier5/sketch.py
@@ -1,6 +1,25 @@
 from py5 import Sketch as py5Sketch
+from py5.mixins import (
+    DataMixin,
+    MathMixin,
+    PixelMixin,
+    PrintlnStream,
+    ThreadsMixin,
+)
 
 __all__ = ("Sketch",)
 
 
-class Sketch(py5Sketch): ...
+# Create a pure version of the py5's Sketch.
+# This approach provides an easier way to eliminate unwanted mixins - behaviours.
+py5Sketch.__bases__ = tuple(base for base in py5Sketch.__bases__ if base.__name__ == "Py5Base")
+
+
+class Sketch(
+    MathMixin,
+    DataMixin,
+    ThreadsMixin,
+    PixelMixin,
+    PrintlnStream,
+    py5Sketch,
+): ...
